7fafd56918654281e89e33537de9554f87d66ad4
Anshu Vishwakarma <anshu.vishwakarma@mysetu.com>
2024-07-02 08:02:25 +0530

    OX

--- toDo/src/main.jsx
+++ toDo/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import OX from './OX.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <OX/>
   </React.StrictMode>,
 )
